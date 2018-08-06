52d9edd5f760036dbe0c1f9450d6ced0374cdd95
tomhartley <tomhartley@rocketmail.com>
2018-08-06 15:11:24 +0100

    Added Python 3 support and refactored
    Refactoring included moving to gpiozero library on python

--- tts.py
+++ tts.py
@@ -7,7 +7,7 @@
 #
 # Written for Raspberry Pi.
 #
-# Bare Conductive code written by Szymon Kaliski.
+# Bare Conductive code written by Szymon Kaliski and Tom Hartley.
 #
 # This work is licensed under a MIT license https://opensource.org/licenses/MIT
 #
@@ -35,13 +35,13 @@
 
 from subprocess import call
 from time import sleep
-import signal, sys, MPR121
-import RPi.GPIO as GPIO
+import MPR121
+from gpiozero import RGBLED
 
 try:
   sensor = MPR121.begin()
 except exception as e:
-  print e
+  print (e)
   sys.exit(1)
 
 num_electrodes = 12
@@ -56,34 +56,7 @@ release_threshold = 20
 sensor.set_touch_threshold(touch_threshold)
 sensor.set_release_threshold(release_threshold)
 
-# handle ctrl+c gracefully
-def signal_handler(signal, frame):
-  light_rgb(0, 0, 0)
-  sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-# set up led
-red_led_pin = 6
-green_led_pin = 5
-blue_led_pin = 26
-
-def light_rgb(r, g, b):
-  # we are inverting the values, because the led is active low
-  # low - on
-  # high - off
-  GPIO.output(red_led_pin, not r)
-  GPIO.output(green_led_pin, not g)
-  GPIO.output(blue_led_pin, not b)
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-GPIO.setup(red_led_pin, GPIO.OUT)
-GPIO.setup(green_led_pin, GPIO.OUT)
-GPIO.setup(blue_led_pin, GPIO.OUT)
-
-light_rgb(0, 0, 0)
+led = RGBLED(6, 5, 26, active_high=False)
 
 # run espeak from shell
 def speak(text):
@@ -94,30 +67,33 @@ def speak(text):
 texts = []
 for i in range(num_electrodes):
   path = "texts/TEXT{0:03d}.txt".format(i)
-  print "loading file: " + path
+  print ("loading file: " + path)
 
   text = open(path, 'r').read()
   texts.append(text)
 
-while True:
-  if sensor.touch_status_changed():
-    sensor.update_touch_data()
-    is_any_touch_registered = False
+running = True
+while running:
+  try:
+    if sensor.touch_status_changed():
+      sensor.update_touch_data()
+      is_any_touch_registered = False
 
-    # check if touch is registred to set the led status
-    for i in range(num_electrodes):
-      if sensor.is_new_touch(i) and not is_any_touch_registered:
-        # play sound associated with that touch
-        print "speaking text: " + texts[i]
-        speak(texts[i])
-      if sensor.get_touch_data(i):
-        is_any_touch_registered = True
+      # check if touch is registred to set the led status
+      for i in range(num_electrodes):
+        if sensor.is_new_touch(i) and not is_any_touch_registered:
+          # play sound associated with that touch
+          print ("speaking text: " + texts[i])
+          speak(texts[i])
+        if sensor.get_touch_data(i):
+          is_any_touch_registered = True
 
-    # light up red led if we have any touch registered currently
-    if is_any_touch_registered:
-      light_rgb(1, 0, 0)
-    else:
-      light_rgb(0, 0, 0)
-
-  # sleep a bit
-  sleep(0.01)
+      # light up red led if we have any touch registered currently
+      if is_any_touch_registered:
+        led.color = (1, 0, 0)
+      else:
+        led.color = (0, 0, 0)
+    sleep(0.01)
+  except KeyboardInterrupt:
+    led.off()
+    running = False
